b6d0e59ec557890e31041857572bb99471e5a3b1
Nicolás Olmos <yohannicolas2003@gmail.com>
2023-10-21 00:45:07 -0500

    initial commit

---
+++ src/main/java/com/example/apimotorinferencia/ApiMotorInferenciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.apimotorinferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMotorInferenciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMotorInferenciaApplication.class, args);
+    }
+
+}
